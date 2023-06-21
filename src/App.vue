13c2e7a1646bf2a680acaa41c0c680349a1049a7
Marcin Bieniek <marcingim@gmail.com>
2023-06-21 11:33:05 +0200

    Add CartStore and gather data from cart

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
 import { useProductStore } from "@/stores/ProductStore";
+import { useCartStore } from "@/stores/CartStore"
 
+const cartStore = useCartStore();
 const productStore = useProductStore();
-
 productStore.fill()
 
 </script>
@@ -17,6 +18,7 @@ productStore.fill()
         v-for="product in productStore.products"
         :key="product.name"
         :product="product"
+        @addToCart="cartStore.addItems($event, product)"
       />
     </ul>
   </div>
